171f5c819fe1bb04972f18c566858d861e1e6aec
Araj Ali <arajali979@gmail.com>
2025-09-19 11:53:59 +0600

    service working

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,30 @@ Mobile Menu Js
 		$("select").niceSelect();
 	}
 
+	// Project Hover active change
+	if ($(".project-wrapper-three").length) {
+		$(".project_item").hover(function () {
+			// Remove active class from all siblings
+			$(this).siblings(".project_item").removeClass("active");
+
+			// Add active class to hovered item
+			$(this).addClass("active");
+
+			// Update image dynamically
+			const newSrc = $(this).data("src");
+			const $image = $(".project_list_img img");
+
+			// Animate zoom out, change image, then zoom back in
+			$image
+				.fadeOut(300)
+				.css("transform", "scale(0.9)")
+				.promise()
+				.done(function () {
+					$image.attr("src", newSrc).fadeIn(300).css("transform", "scale(1)");
+				});
+		});
+	}
+
 	// Accordion Js
 	if ($(".accordion-item").length > 0) {
 		$(".accordion-item .faq-title").on("click", function () {
